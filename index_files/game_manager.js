993bbc5080f269a1bc3f515eee12982e16db6745
true65536 <66752659+true65536@users.noreply.github.com>
2020-06-22 13:36:39 -0700

    Change html title

--- index_files/game_manager.js
+++ index_files/game_manager.js
@@ -130,8 +130,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value ;
 
-          // The mighty 2584 tile
-          if (merged.value === 2584) self.won = true;
+          // The mighty 5702887 tile
+          if (merged.value === 5702887) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
